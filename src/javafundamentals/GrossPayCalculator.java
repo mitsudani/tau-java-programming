ace16ca7c4f3069c246a464123431b9f13beeed9
Mitsudani <mdsachetti@gmail.com>
2023-03-05 02:38:32 -0300

    First commit

---
+++ src/javafundamentals/GrossPayCalculator.java
@@ -0,0 +1,15 @@
+package javafundamentals;
+
+public class GrossPayCalculator {
+
+    // default method in Java
+    public static void main(String arg[]) {
+        // 1. Get the number of hours worked
+
+        // 2. Get the hourly pay rate
+
+        // 3. Multiply hours and pay rate
+
+        // 4. Display result
+    }
+}
