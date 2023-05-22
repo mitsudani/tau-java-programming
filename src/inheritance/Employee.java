4cea0f6fbba9add0623cf61952cdbf9d225ff7df
Mitsudani <mdsachetti@gmail.com>
2023-05-21 21:15:43 -0300

    Inheritance

---
+++ src/inheritance/Employee.java
@@ -0,0 +1,24 @@
+package inheritance;
+
+public class Employee {
+
+    private String employeeId;
+    private String title;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
---
+++ src/inheritance/InheritanceTester.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class InheritanceTester {
+
+    public static void main(String args[]) {
+
+        Person person = new Person();
+        Employee employee = new Employee();
+
+    }
+}
