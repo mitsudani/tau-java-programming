cb0dbf02c409fffb9c7c2d07af895ae61d094270
Mitsudani <mdsachetti@gmail.com>
2023-06-08 00:13:02 -0300

    Limiting access in inheritance

--- src/inheritance/InheritanceTester.java
+++ src/inheritance/InheritanceTester.java
@@ -4,15 +4,19 @@ public class InheritanceTester {
 
     public static void main(String args[]) {
 
-        //Person person = new Person();
-        Employee employee = new Employee();
+//        Person person = new Person();
+//        Employee employee = new Employee();
+//
+//        Rectangle rectangle = new Rectangle();
+//        rectangle.print();
+//
+//        Square square = new Square();
+//        square.print();
+//        square.print("square");
 
-        Rectangle rectangle = new Rectangle();
-        rectangle.print();
-
-        Square square = new Square();
-        square.print();
-        square.print("square");
+        Mother mom = new Mother();
+        mom.setName("Cristina");
+        System.out.println(mom.getName() + " is a " + mom.getGender());
 
     }
 }
---
+++ src/inheritance/Woman.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class Woman extends Person {
+
+    public Woman() {
+        setGender("female");
+    }
+
+}
